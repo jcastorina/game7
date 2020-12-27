b02719ac00a46c8e4145e1970bf840c1858345a2
Mike Allen <mikeleonardallen@gmail.com>
2020-12-27 16:30:08 -0700

    move things into dialog

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -2,19 +2,46 @@ import { FC } from "react";
 import { makeStyles } from '@material-ui/core/styles';
 
 type Props = {
+  character: string;
   dialog: string;
   color: string[];
+  onClick: () => void;
 }
-const Dialog: FC<Props> = ({dialog, color}) => {
+const Dialog: FC<Props> = ({dialog, character, color, onClick}) => {
   const styles = useStyles();
-  return <div className={styles.dialog} style={{color: `${color}`}}>{dialog || null}</div>
+  return (
+    <div className={styles.viewport} onClick={onClick} >
+      <img className={styles.avatar} src={character} alt="char" />
+      <div className={styles.text} style={{color: `${color}`}}>{dialog}</div>
+    </div>
+  )
 }
 
 const useStyles = makeStyles({
-  dialog: {
+  viewport: {
+    border: '1px solid silver',
+    backgroundColor: 'rgba(0, 0, 0, 0.66)',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '80vw',
+    overflow: 'hidden',
+    // -moz-user-select: none;
+    // -webkit-user-select: none;
+    '&:hover': {
+      backgroundColor: 'rgba(0, 0, 0, 0.7)',
+    }
+  },
+  avatar: {
+    height: '40vh',
+    // -moz-user-select: none;
+    // -webkit-user-select: none;
+  },
+  text: {
     fontSize: 'x-large',
     padding: '32px',
-  }
+  },
 });
 
 export default Dialog;
--- src/components/Screen.tsx
+++ src/components/Screen.tsx
@@ -1,17 +1,13 @@
 import { useState } from "react";
-import { makeStyles } from '@material-ui/core/styles';
-
 import { IState } from '../types';
 import { useMachine } from '../hooks';
-import Avatar from '../components/Avatar';
 import Background from '../components/Background';
 import Dialog from '../components/Dialog';
 
 const Screen = <T extends string>({ state: state$ }: {
   state: IState<T>;
 }) => {
-
-  const styles = useStyles();
+  
   const [count, setCount] = useState<number>(0);
   const [state, setRunLevel] = useMachine(state$);
   
@@ -30,30 +26,9 @@ const Screen = <T extends string>({ state: state$ }: {
 
   return (
     <Background background={background$}>
-      <div className={styles.viewport} onClick={handleClick} >
-        <Avatar character={character$} />
-        <Dialog dialog={dialog$} color={dialogColor} />
-      </div>
+      <Dialog onClick={handleClick} character={character$} dialog={dialog$} color={dialogColor} />
     </Background>
   )
 }
-
-const useStyles = makeStyles({
-  viewport: {
-    border: '1px solid silver',
-    backgroundColor: 'rgba(0, 0, 0, 0.66)',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '80vw',
-    overflow: 'hidden',
-    // -moz-user-select: none;
-    // -webkit-user-select: none;
-    '&:hover': {
-      backgroundColor: 'rgba(0, 0, 0, 0.7)',
-    }
-  },
-});
 
 export default Screen;
